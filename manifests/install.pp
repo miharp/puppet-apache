3f425dfbe94ff724f42cb9177e69fff2a9934b26
root <root@miharp1c.mylabserver.com>
2019-12-31 18:28:06 +0000

    Added config class to manage configuration files

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,8 +4,7 @@
 #
 # @example
 #   include apache::install
-class apache::install (
-) {
+class apache::install {
   package { "${apache::install_name}":
     ensure => $apache::install_ensure,
   }
